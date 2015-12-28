8b04ec1b4fc291d6412bd704dab65cb4ce67d6a1
ich <ich@ich.ich>
2015-12-28 17:28:23 +0100

    Initial commit

---
+++ app/src/androidTest/java/de/smart_efb/efbapp/smartefb/ApplicationTest.java
@@ -0,0 +1,13 @@
+package de.smart_efb.efbapp.smartefb;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/test/java/de/smart_efb/efbapp/smartefb/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package de.smart_efb.efbapp.smartefb;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
