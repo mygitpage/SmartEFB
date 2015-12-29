8effa181aa72bd0d7f6813cbfddc314823c2b4ad
ich <ich@ich.ich>
2015-12-30 00:42:59 +0100

    Änderung vom 30.12.2015

--- app/src/main/java/de/smart_efb/efbapp/smartefb/MainActivity.java
+++ app/src/main/java/de/smart_efb/efbapp/smartefb/MainActivity.java
@@ -8,8 +8,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int NUM_COLS = 5;
+    private static final int NUM_ROWS = 15;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +25,60 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        populateButtons();
+        
+        
+    }
+
+    private void populateButtons() {
+
+        TableLayout table = (TableLayout) findViewById(R.id.tableForButtons);
+
+
+        for (int row=0; row < NUM_ROWS; row++) {
+
+            TableRow tableRow = new TableRow(this);
+            tableRow.setLayoutParams(new TableLayout.LayoutParams(
+                    TableLayout.LayoutParams.MATCH_PARENT,
+                    TableLayout.LayoutParams.MATCH_PARENT,
+                    1.0f));
+
+            table.addView(tableRow);
+
+            for (int col=0; col < NUM_COLS; col++) {
+
+                final int FINAL_COL = col;
+                final int FINAL_ROW = row;
+
+                Button button = new Button(this);
+
+                button.setLayoutParams(new TableRow.LayoutParams(
+                        TableRow.LayoutParams.MATCH_PARENT,
+                        TableRow.LayoutParams.MATCH_PARENT,
+                        1.0f));
+
+                button.setText("" + col + "," + row);
+                // Sorgt dafuer, dass bei kleinen Buttons der Text reinpasst
+                button.setPadding(0, 0, 0, 0);
+
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        gridButtonClicked( FINAL_COL, FINAL_ROW);
+                    }
+                });
+
+                tableRow.addView(button);
+
             }
-        });
+
+        }
+    }
+
+    private void gridButtonClicked(int x, int y) {
+
+        Toast.makeText(this,"Button clicked: " + x + "," + y , Toast.LENGTH_SHORT).show();
+
     }
 
     @Override
