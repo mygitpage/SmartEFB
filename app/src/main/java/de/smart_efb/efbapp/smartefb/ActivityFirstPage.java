0ec7d4f7b9c2426d013496a0c34238e41b798465
mygitpage <jjhh2000@gmx.de>
2016-02-14 23:43:09 +0100

    14022016

--- app/src/main/java/de/smart_efb/efbapp/smartefb/ActivityFirstPage.java
+++ app/src/main/java/de/smart_efb/efbapp/smartefb/ActivityFirstPage.java
@@ -1,11 +1,14 @@
 package de.smart_efb.efbapp.smartefb;
 
 import android.app.ActionBar;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ActivityFirstPage extends AppCompatActivity {
 
@@ -15,6 +18,22 @@ public class ActivityFirstPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity_first_page);
+
+
+        Button buttonConnectBook = (Button) findViewById(R.id.btnConnectBook);
+
+        buttonConnectBook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(), ActivityConnectBook.class);
+
+                startActivity(intent);
+
+            }
+        });
+
+
 
 
     }
